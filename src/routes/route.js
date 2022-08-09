94936d615f1b6742bbc143f3f83a286e104e1a97
Tanweer Zaya <104303869+zayatanweer@users.noreply.github.com>
2022-08-09 12:02:18 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const underscore = require('underscore')
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -14,6 +15,39 @@ router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
+
+router.get('/movies', function (req,res){
+    let movies = ["foreinsic", "saw", "tare zameen pe"]
+    res.send(movies)
+    console.log(movies)
+})
+
+router.get('/movies/:indexNumber', function (req,res){
+    let movies = ["3-idiots", "dangal", "sultan", "my name is khan"]
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    let requiredMovies = movies[movieIndex]
+    res.send(requiredMovies)
+})
+
+router.get('/films', function (req,res){
+    const films = [{
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       res.send(films)
+       console.log(films)
+       
+});
 
 router.get('/student-details/:name', function(req, res){
     /*
