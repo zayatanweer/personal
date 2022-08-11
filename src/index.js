782ff4ab327601db82aef447f21e70dd7d0138cc
Tanweer Zaya <104303869+zayatanweer@users.noreply.github.com>
2022-08-11 23:13:45 +0530

    integer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,70 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 
-const route = require('./routes/route.js');
+const route = require("./routes/route.js");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use("/", route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
 
+
+
+app.get("/sol1", function (req, res) {
+
+  let arr = [1, 2, 3, 5, 6, 7];
+  totalNum = arr[arr.length - 1];
+  let missingNum = [];
+  for (let i = 1; i < totalNum; i++) {
+    if (arr.indexOf(i) == -1) {
+      missingNum.push(i);
+    }
+  }
+  res.send(missingNum.toString());
+
+//   let total = 0;
+//   for (var i in arr) {
+//       total += arr[i];
+//   }
+
+//   let lastDigit= arr.pop()
+//   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+//   let missingNumber= consecutiveSum - total
+//   res.send(  { data: missingNumber  }  );
+
+  });
+
+
+app.get("/sol2", function (req, res) {
+  let arr = [33, 34, 35, 37, 38];
+  
+  totalNum = arr[arr.length -1];
+  let missingNum = [];
+  for (let i = 33; i < totalNum; i++){
+    if (arr.indexOf(i) == -1){
+        missingNum.push(i);
+    }
+  }
+  res.send(missingNum.toString());
+
+
+//   let len = arr.length;
+
+//   let total = 0;
+//   for (var i in arr) {
+//     total += arr[i];
+//   }
+
+//   let firstDigit = arr[0];
+//   let lastDigit = arr.pop();
+//   let consecutiveSum = ((len + 1) * (firstDigit + lastDigit)) / 2;
+//   let missingNumber = consecutiveSum - total;
+
+//   res.send({ data: missingNumber });
+});
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,107 +1,90 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+router.get("/random" , function(req, res) {
+    res.send("hi there")
+})
+
+
+router.get("/test-api" , function(req, res) {
+    res.send("hi FunctionUp")
+})
+
+
+router.get("/test-api-2" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
+
+
+router.get("/test-api-3" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+
+
+router.get("/test-api-5" , function(req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+router.get("/test-api-6" , function(req, res) {
+    res.send({a:56, b: 45})
+})
+
+router.post("/test-post", function(req, res) {
+    res.send([ 23, 45 , 6])
+})
+
+
+router.post("/test-post-2", function(req, res) {
+    res.send(  { msg: "hi" , status: true }  )
 });
 
-router.get("/movies/:indexNumber", function(req, res){
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index value. less than 0 or greater than array (length - 1) are not valid
-    if(movieIndex<0 || movieIndex>=movies.length) {
-        //if the index is invalid send an error message
-        return res.send('The index value is not correct, Please check the it')
-    }
+let persons = [
+    {
+        name : "PK",
+        age : 10,
+        votingStatus : false
+    },
+    {
+        name : "SK",
+        age : 20,
+        votingStatus : false
+    },
+    {
+        name : "AA",
+        age : 70,
+        votingStatus : false
+    },
+    {
+        name : "SC",
+        age : 5,
+        votingStatus : false
+    },
+    {
+        name : "HO",
+        age : 40,
+        votingStatus : false
+    },
+];
 
-    //if the index was valid send the movie at that index in response
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
+router.post('/persons', function(req,res){
+    persons.forEch((person, i )=> {
+        if (persons[i].age < 10 ){
+            return true
+        }
+        else return false
+    })
+   req.send() 
 })
-
-router.get("/shoes", function(req, res){
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
-})
-
-// uses query params
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-// use path param
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-       //send all the films
-      res.send(films) 
-})
-
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-
-       let filmId = req.params.filmId
-
-       //iterate all the films
-       //search for a film whose id matches with the id recevied in request
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-               //if there is a match return the response from here
-               return res.send(film)
-           }
-       }
-
-       //if there is no match give an error response
-       res.send("The film id doesn't match any movie")
-})
-
 module.exports = router;
-// adding this comment for no reason
