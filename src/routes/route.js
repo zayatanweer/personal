6dbd8621efb0efd313d7b470e134324b481635ad
Tanweer Zaya <104303869+zayatanweer@users.noreply.github.com>
2022-08-16 21:54:06 +0530

    post api 2 update

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,84 +7,76 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
 
+// WRITE A POST API TO THE ABOVE take this as sample for array of persons :
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+//  you will be given an array of persons ( 1 .e an array of objects )
+//  .. each person will have a ( name : String , age : Number , votingStatus : true / false
+//  ( BooLean take input in query param as votingAge..and for all the people above that age ,
+//    change votingStatus as true also return an array consisting of only the person that can
 
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-});
-
-let persons = [
+let persons= [
     {
-        name : "PK",
-        age : 10,
-        votingStatus : false
-    },
-    {
-        name : "SK",
-        age : 20,
-        votingStatus : false
-    },
-    {
-        name : "AA",
-        age : 70,
-        votingStatus : false
-    },
-    {
-        name : "SC",
-        age : 5,
-        votingStatus : false
-    },
-    {
-        name : "HO",
-        age : 40,
-        votingStatus : false
-    },
-];
+    name: "AA",
+    age: 20,
+    votingStatus: false
+  },
+  {
+    name: "BB",
+    age: 25,
+    votingStatus: false
+  },
+  {
+    name: "CC",
+    age: 23,
+    votingStatus: false
+  },
+  {
+    name: "DD",
+    age: 25,
+    votingStatus: false
+  },
+  {
+      name: "EE",
+      age: 22,
+      votingStatus: false
+  },
+  {
+      name : "FF",
+      age: 80,
+      votingStatus: false
+  },
+  {
+    name: "GG",
+    age: 40,
+    votingStatus: false
+  }
+  ]
 
-router.post('/persons', function(req,res){
-    persons.forEch((person, i )=> {
-        if (persons[i].age < 10 ){
-            return true
+// router.post('/persons', function(res,req){
+// inputAge = req.query.votingAge;
+// persons.map(person =>(person.age > inputAge) ? person.votingStatus = true : person.votingStatus);
+// let result = persons.filter(newPer => {if(newPer.votingStatus==true){return newPer.name}})
+
+// res.send({data:result})
+// })
+
+
+  router.post('/persons', function(req,res) {
+  
+      let param = req.query;
+      let age = param.age;
+      let elegiblePerson = [];
+      
+     for(let i =0; i < persons.length ; i++){
+        if(persons[i].age >= age){
+             persons[i].votingStatus = true
+            elegiblePerson.push(persons[i])  
         }
-        else return false
-    })
-   req.send() 
-})
+     }
+  
+    res.send(elegiblePerson)
+            
+  })
+
 module.exports = router;
